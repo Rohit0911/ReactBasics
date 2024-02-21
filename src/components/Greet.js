eca722943e5492cada7996376c0fd9f82c40fce7
rohitpol0911@gmail.com <rohitpol0911@gmail.com>
2024-02-21 20:38:28 +0530

    Basics

---
+++ src/components/Greet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+/* function Greet(){
+    return <h2>Hello this Rohit Pol</h2>
+   }
+
+   
+   //keyword default allows us to import it as an name...
+   */
+const Greet =()=><h3> Hello This is Rohit from Arrow </h3>
+export default Greet
+
+
+// export const Greet=()=><h2>Hello Hi</h2> ... One way of exporting
+
+
+
