447d47f32fad90cef018afacf1d51f16a5851b9f
rohitpol0911@gmail.com <rohitpol0911@gmail.com>
2024-02-21 22:54:33 +0530

    4

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+//   return (
+//     <div>
+//         <h1>Hello Rohit</h1>
+//     </div>
+//   )
+return React.createElement('div',null,'Hello Rohit')
+}
+
+
+export default Hello
