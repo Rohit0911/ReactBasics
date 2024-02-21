680068af900c13f91e42b3468f4c4648a5f2f304
rohitpol0911@gmail.com <rohitpol0911@gmail.com>
2024-02-21 22:31:29 +0530

    3

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+
+class Welcome extends Component{
+
+    render(){
+        return <h3>Hello from class component!!</h3>
+    }
+
+}
+
+
+export default Welcome
